b54c01c3f211f11e73c814e40e2ff39fd551b560
Zachary Briggs <zacharybriggs1@hotmail.com>
2017-10-11 10:49:02 -0500

    Fixed Item.h's comments

--- Wumpus/Item.h
+++ Wumpus/Item.h
@@ -14,19 +14,21 @@ public:
 	Protection: Public
 	*/
 	bool CheckForPlayer(Entity* dude);
-	/*Prototype: Point2D GetPosition();
-	Description: Returns the Item's mPosition.
-	Arguments: None.
-	Precondition: None
-	Postcondition: Item's mPosition is returned.
+	/*Prototype: bool CheckForPlayer(Entity* dude);
+	Description: Checks to see an Entity is occupying the same position as the Item.
+	Arguments: An entity.
+	Precondition: None.
+	Postcondition: Returns true if an Entity is occupying the same space. Returns false
+	if it isn't.
 	Protection: Public
 	*/
 	bool IsPlayerNearby(Entity* dude);
-	/*Prototype: Point2D GetPosition();
-	Description: Returns the Item's mPosition.
-	Arguments: None.
+	/*Prototype: bool IsPlayerNearby(Entity* dude);
+	Description: Checks to see if an Entity is in an adjacent position.
+	Arguments: The player.
 	Precondition: None
-	Postcondition: Item's mPosition is returned.
+	Postcondition: Returns true if an Entity is occupying an adjacent position. Returns
+	false if they aren't.
 	Protection: Public
 	*/
 };
